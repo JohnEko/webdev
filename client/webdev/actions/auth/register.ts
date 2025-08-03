dbff660c4b83153a7f5c102d4d3166d36fc48600
JohnEko <efezino10@gmail.com>
2025-08-03 16:32:26 +0100

    db added

---
+++ client/webdev/actions/auth/register.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import bcrypt from "bcryptjs";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/lib/user";
+import { RegisterSchema, RegisterSchemaType } from "@/schemas/RegisterSchema";
+
+
+
+export const signUp = async (values: RegisterSchemaType) => {
+  const validatedFields = RegisterSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return { error: "Invalid fields" };
+  }
+
+  const { name, email, password } = validatedFields.data;
+
+  //user email will be unique
+  const user = await getUserByEmail(email)
+
+  if(user){
+    return {error: "Email already in use!"}
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword   
+    },
+  });
+  return {success: "User Created"}
+};
--- client/webdev/app/(auth)/register/page.tsx
+++ client/webdev/app/(auth)/register/page.tsx
@@ -1,8 +1,11 @@
 import RegisterForm from '@/components/auth/RegisterForm'
 import Container from '@/components/layout/Container'
+import { db } from '@/lib/db'
 import React from 'react'
 
-const Register = () => {
+const Register = async () => {
+
+  await db.user
   return (
     <Container>
       <RegisterForm />
