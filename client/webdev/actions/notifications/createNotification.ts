61a275f7d4bf512fcdcedf0be87c9210bcd0fe65
JohnEko <efezino10@gmail.com>
2025-09-07 16:35:44 +0100

    bug notification

---
+++ client/webdev/actions/notifications/createNotification.ts
@@ -0,0 +1,65 @@
+'use server'
+
+import { auth } from "@/auth"
+import { db } from "@/lib/db"
+import { EntityType, NotificationType } from "@prisma/client"
+
+
+export const createNotification = async ({
+    recipientId,
+    type,
+    blogId,
+    commentId,
+    content,
+    entityType,
+   
+}: {
+    recipientId: string
+    type: NotificationType
+    blogId?: string
+    commentId?: string
+    entityType?: EntityType
+    content?: string
+}) => {
+
+    const session = await auth()
+
+    if(!session?.user){
+        return {error: "Not authenticated"}
+    }
+
+    if(session.user.userId == recipientId){
+        return {error: "Failed to send Notification to self"}
+    }
+
+    const recipient = await db.user.findUnique({
+        where: {id: recipientId}
+    })
+
+    if(!recipient){
+        return {error: "Recipient not found"}
+    }
+
+    try {
+        
+        await db.notification.create({
+        data: {
+            senderId: session.user.userId,
+            recipientId,
+            type,
+            blogId: blogId ?? '',
+            commentId: commentId ?? '',
+            entityType,
+            content: content ?? ''
+
+        },
+    })
+    return {success: "Notification sent"}
+        
+    } catch (error) {
+        console.error("Error creating notification:", error)
+        return {error: "Failed to send notification"}
+    }
+    
+    
+}
